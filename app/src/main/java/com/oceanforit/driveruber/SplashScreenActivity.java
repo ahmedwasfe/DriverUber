1e25905888b34d53a0eacc21ef25f180f1ef56eb
ahmet <ahmedwasfe84@gmail.com>
2020-07-13 13:49:29 +0300

    Initial commit

---
+++ app/src/main/java/com/oceanforit/driveruber/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.oceanforit.driveruber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.TimeUtils;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Completable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Action;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        delaySplashScreen();
+    }
+
+    private void delaySplashScreen() {
+
+        Completable.timer(5, TimeUnit.SECONDS,
+                AndroidSchedulers.mainThread())
+                .subscribe(new Action() {
+                    @Override
+                    public void run() throws Exception {
+                        Toast.makeText(SplashScreenActivity.this, "Splash Screen done!!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
